5d50b709835622be01668c96b401e2f66b0e8843
Jonathan Baldie <jon@jonbaldie.com>
2026-06-25 09:21:32 +0100

    feat: Add messgo quality gate to CI and dummy violation

---
+++ dummy.go
@@ -0,0 +1,24 @@
+package main
+
+func DummyComplexFunction() {
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+    if true {}
+}
